8a70872d0c5767aeacc6238b97d8668192fe2505
davide92 <thechryst92@gmail.com>
2019-04-03 14:57:32 +0200

    add canvas margin

--- src/plot_graph.c
+++ src/plot_graph.c
@@ -8,6 +8,8 @@
 #include "plot_graph.h"
 #include "utils.h"
 
+void set_canvas(FILE *gnuplot_pipe);
+
 void plot_instance(Tsp_prob *inst) {
 
     DEBUG_PRINT(("\n%s\n", "--start plot_instance method"));
@@ -37,30 +39,16 @@ void plot_instance(Tsp_prob *inst) {
     fprintf(gnuplot_pipe, "%s\n", "set terminal postscript portrait size 10, 8 \
 enhanced color \"Helvetica\" 8");
 
-    //fprintf(gnuplot_pipe, "%s \n", "set size 1,1"); //set size of canvas
-
     //output file
     fprintf(gnuplot_pipe, "%s\n", "set output 'graph/nodes.eps'");
 
-    //fprintf(gnuplot_pipe, "%s\n", "set title 'Graph nodes'"); //plot title
-
-    //fprintf(gnuplot_pipe, "%s\n", "set xlabel 'X'");
-
-    //fprintf(gnuplot_pipe, "%s\n", "set ylabel 'Y'");
-
-    //remove border
-    fprintf(gnuplot_pipe, "%s\n", "unset border");
-
-    //remove x-axis
-    fprintf(gnuplot_pipe, "%s\n", "unset xtics");
-
-    //remove y-axis
-    fprintf(gnuplot_pipe, "%s\n", "unset ytics");
-
-    //fprintf(gnuplot_pipe, "%s\n", "set offset 1,1,1,1");
+    //set canvas
+    set_canvas(gnuplot_pipe);
 
     //plot path with point style and label
     fprintf(gnuplot_pipe, "%s\n", "plot 'graph/data.dat' with labels point pointtype 7 offset char 1,-1.0 notitle");
+
+    fprintf(gnuplot_pipe,"%s\n", "quit");
 
     pclose(gnuplot_pipe);
 
@@ -189,12 +177,41 @@ void plot_edges(Solution_list *edges_list, Tsp_prob * instance) {
     //set teminal type with parameters, size in inch
     fprintf(gnuplot_pipe, "%s\n", "set terminal postscript portrait size 10, 8 enhanced color \"Helvetica\" 7");
 
-    //fprintf(gnuplot_pipe, "%s \n", "set size 1,1"); //set size of canvas
-
     //output file
     fprintf(gnuplot_pipe, "%s\n", "set output 'graph/path.eps'");
 
-    //fprintf(gnuplot_pipe, "%s\n", "set title 'TSP"); //plot title
+     //set canvas
+     set_canvas(gnuplot_pipe);
+
+    //set style of the lines
+    fprintf(gnuplot_pipe, "%s\n",  "set style line 1 \
+									linecolor rgb '#0060ad' \
+									linetype 1 linewidth 1" );
+
+    char *model_name = calloc(64, 1);
+    inverse_map_model_type(instance->model_type, model_name);
+
+    fprintf(gnuplot_pipe, "set title \"%s using %s\"\n", instance->name, model_name);
+
+    free(model_name);
+    //plot path with point style and label
+    fprintf(gnuplot_pipe, "%s\n", "plot 'graph/path.dat' with linespoints linestyle 1, '' with labels offset char 1,-1.0 point pointtype 7 lc rgb '#0060ad' notitle");
+
+    fprintf(gnuplot_pipe,"%s\n", "quit");
+
+    pclose(gnuplot_pipe);
+
+    remove("graph/path.dat");
+
+    printf("\n%s\n", "--plot completed");
+
+}
+
+void set_canvas(FILE *gnuplot_pipe) {
+
+    //fprintf(gnuplot_pipe, "%s \n", "set size 1,1"); //set size of canvas
+
+    //fprintf(gnuplot_pipe, "%s\n", "set title 'Graph nodes'"); //plot title
 
     //fprintf(gnuplot_pipe, "%s\n", "set xlabel 'X'");
 
@@ -212,27 +229,13 @@ void plot_edges(Solution_list *edges_list, Tsp_prob * instance) {
     //remove legend
     fprintf(gnuplot_pipe, "%s\n", "unset key");
 
+    //set margin
+    fprintf(gnuplot_pipe, "%s\n", "set lmargin at screen 0.05");
+    fprintf(gnuplot_pipe, "%s\n", "set rmargin at screen 0.95");
+    fprintf(gnuplot_pipe, "%s\n", "set bmargin at screen 0.05");
+    fprintf(gnuplot_pipe, "%s\n", "set tmargin at screen 0.95");
+
+
     //fprintf(gnuplot_pipe, "%s\n", "set offset 1,1,1,1");
-
-    //set style of the lines
-    fprintf(gnuplot_pipe, "%s\n",  "set style line 1 \
-									linecolor rgb '#0060ad' \
-									linetype 1 linewidth 1" );
-
-    char *model_name = calloc(64, 1);
-    inverse_map_model_type(instance->model_type, model_name);
-
-    fprintf(gnuplot_pipe, "set title \"%s using %s\"\n", instance->name, model_name);
-
-    free(model_name);
-    //plot path with point style and label
-    fprintf(gnuplot_pipe, "%s\n", "plot 'graph/path.dat' with linespoints linestyle 1, '' with labels offset char 1,-1.0 point pointtype 7 lc rgb '#0060ad' notitle");
-
-    pclose(gnuplot_pipe);
-
-    remove("graph/path.dat");
-
-    printf("\n%s\n", "--plot completed");
-
 }
 
